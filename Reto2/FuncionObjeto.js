8f57dd5a4df65b364056a8158d59fb4880cb8a1f
DianaBedoya <dianabedoya570@gmail.com>
2022-11-18 19:56:22 -0500

    corregir la funcion

--- Reto2/FuncionObjeto.js
+++ Reto2/FuncionObjeto.js
@@ -2,7 +2,9 @@ function myFunction(a, b) {
     const  atributos=Object.keys(a);
     for(let i=0;i<atributos.length;i++){
         if(atributos[i]===b){
+          if( a[atributos[i]] !== undefined){
             return true;
+          }
         }
     }
     return false;
@@ -12,13 +14,3 @@ function myFunction(a, b) {
   console.log(myFunction({x:'a',y:'b',z:'c'},'a'))
   console.log(myFunction({x:'a',y:'b',z:undefined},'z'))
 
-function incrementItems(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] =arr[i] + 1
-  }
-
-  return arr
-}
-console.log(incrementItems([0, 1, 2, 3]))	
-console.log(incrementItems([2, 4, 6, 8]))	
-console.log(incrementItems([-1, -2, -3, -4]))	
