fa3f993bd58eccda33d45545cd8a694d5193ec3a
DianaBedoya <dianabedoya570@gmail.com>
2022-11-24 20:28:11 -0500

    se adiciona la corrección a la función que compara los arreglos

--- Reto2JS/FixError.js
+++ Reto2JS/FixError.js
@@ -19,7 +19,12 @@ checkEquals([1, 2], [1, 2]) ➞ false
 // Yikes! What happened? */
 
 function checkEquals(arr1, arr2) {
-    if (arr1 === arr2) {
+    if (arr1.length === arr2.length) {
+        for(let i=0;i<arr1.length;i++){
+            if(arr1[i]!==arr2[i]){
+              return false;
+            }
+        }
       return true
      } else {
       return false
